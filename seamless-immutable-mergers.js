517b8b590a6d3d1999a4086b9971143b5793f56c
Christian Rudh <christian@rudh.se>
2015-05-11 11:49:33 +0200

    Only do a Node export and clean up the code.

--- seamless-immutable-mergers.js
+++ seamless-immutable-mergers.js
@@ -1,52 +1,45 @@
-(function(){
-  "use strict";
+"use strict";
+var immutable = require("immutable");
 
-  function concatArrayMerger(current, other) {
-    if (!(current instanceof Array) || !(other instanceof Array)) return;
+function concatArrayMerger(current, other) {
+  if (!(current instanceof Array) || !(other instanceof Array)) return;
 
-    return current.concat(other);
+  return current.concat(other);
+}
+
+function updatingDiffArrayMerger(current, other, config) {
+  if (!(current instanceof Array) || !(other instanceof Array)) return;
+  if (current.length === 0 || other.length === 0) return;
+
+  var identifier = config.diffArrayMergerIdentifier;
+  if (current[0] === null || !(typeof current[0] === "object") || !current[0][identifier]) return;
+  if (other[0] === null || !(typeof other[0] === "object") || !other[0][identifier]) return;
+
+  var currentMap = {};
+  for (var i = 0; i < current.length; i++) {
+    currentMap[current[i][identifier]] = i;
   }
 
-  function updatingDiffArrayMerger(current, other, config) {
-    if (!(current instanceof Array) || !(other instanceof Array)) return;
-    if (current.length === 0 || other.length === 0) return;
-    if (current[0] === null || !(typeof current[0] === 'object') || !current[0][config.diffArrayMergerIdentifier]) return;
-    if (other[0] === null || !(typeof other[0] === 'object') || !other[0][config.diffArrayMergerIdentifier]) return;
+  var resultList = current.asMutable();
+  for (var j = 0; j < other.length; j++) {
+    var matchingCurrentIndex = currentMap[other[j][identifier]];
 
-    var currentMap = {};
-    for (var i = 0; i < current.length; i++) {
-      currentMap[current[i][config.diffArrayMergerIdentifier]] = i;
+    if (!matchingCurrentIndex) {
+      resultList.push(other[j]);
+    } else {
+      resultList[matchingCurrentIndex] = resultList[matchingCurrentIndex].merge(other[j], config);
     }
-
-    var resultList = current.asMutable();
-    for (var j = 0; j < other.length; j++) {
-      var matchingCurrentIndex = currentMap[other[j][config.diffArrayMergerIdentifier]];
-
-      if (!matchingCurrentIndex) {
-        resultList.push(other[j]);
-      } else {
-        resultList[matchingCurrentIndex] = resultList[matchingCurrentIndex].merge(other[j], config);
-      }
-    }
-
-    return Immutable(resultList);
   }
 
-  // Export the library
-  var ImmutableMergers = {
-    concatArrayMerger: concatArrayMerger,
-    updatingDiffArrayMerger: updatingDiffArrayMerger
-  };
+  return immutable(resultList);
+}
 
-  Object.freeze(ImmutableMergers);
+// Export the library
+var immutableMergers = {
+  concatArrayMerger: concatArrayMerger,
+  updatingDiffArrayMerger: updatingDiffArrayMerger
+};
 
-  if (typeof module === "object") {
-    module.exports = ImmutableMergers;
-  } else if (typeof exports === "object") {
-    exports.Immutable = ImmutableMergers;
-  } else if (typeof window === "object") {
-    window.Immutable = ImmutableMergers;
-  } else if (typeof global === "object") {
-    global.Immutable = ImmutableMergers;
-  }
-})();
+Object.freeze(immutableMergers);
+
+module.exports = immutableMergers;
